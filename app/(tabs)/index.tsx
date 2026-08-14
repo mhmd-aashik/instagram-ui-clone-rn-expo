6dc6a5ecec6f63f3abe747fe7debf49a03b96e69
Mohammed Aashik <aashikdevelop@gmail.com>
2026-08-15 03:33:05 +0400

    Revamp HomeScreen component to include a header with logo and action buttons. Added SafeAreaView for better layout management and updated styles for improved UI consistency.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,49 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Instagram Clone</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.logo}>Instagram</Text>
+
+        <View style={styles.actions}>
+          <TouchableOpacity>
+            <Ionicons name="heart-outline" size={27} color="#000" />
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <Ionicons name="paper-plane-outline" size={26} color="#000" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#ffffff",
+    backgroundColor: "#fff",
   },
 
-  text: {
+  header: {
+    height: 55,
+    paddingHorizontal: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+
+  logo: {
     fontSize: 28,
     fontWeight: "700",
+  },
+
+  actions: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 20,
   },
 });
